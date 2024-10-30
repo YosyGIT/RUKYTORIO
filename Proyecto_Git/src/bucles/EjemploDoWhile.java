5f60673bc3eef2478a6dc94cef5e5aec6395c941
DAW <DAW@AEMCENT-61NQ5U0>
2024-10-30 09:03:26 +0100

    Commit inicial, subí el ejemplo de EjemploDoWhile

---
+++ Proyecto_Git/src/bucles/EjemploDoWhile.java
@@ -0,0 +1,17 @@
+package bucles;
+
+public class EjemploDoWhile {
+	public static void main (String[]args) {
+		int num=0;
+		do {
+		System.out.println("El número es: "+num);	
+		num=num +1;
+			
+		}while(num<10);
+		
+		
+	
+	
+	
+	}
+}
