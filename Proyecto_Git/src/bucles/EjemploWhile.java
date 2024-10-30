58040dff5584a20c95caad0ef3e70bb78b111a5e
DAW <DAW@AEMCENT-61NQ5U0>
2024-10-30 09:22:00 +0100

    El ejemplo While subido por primera vez

---
+++ Proyecto_Git/src/bucles/EjemploWhile.java
@@ -0,0 +1,13 @@
+package bucles;
+
+public class EjemploWhile {
+	public static void main (String[]args) {
+	int cont=0;
+	while(cont<10) {
+		cont=cont +1;
+		System.out.println("El número es: "+ cont);	
+
+		
+		}	
+	}
+}
