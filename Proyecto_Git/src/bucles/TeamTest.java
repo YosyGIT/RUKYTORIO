11254a77da9d5a170289bfa5f29e4832b7ea3ffa
abraham.perbar <abraham.perbar@ED34007007P714.educa.jcyl.es>
2024-11-27 09:44:21 +0100

    Primer Commit Abraham

---
+++ Proyecto_Git/src/bucles/TeamTest.java
@@ -0,0 +1,11 @@
+package bucles;
+
+import javax.management.openmbean.OpenMBeanAttributeInfo;
+
+public class TeamTest {
+  public static void main(String[] args) {
+	for (int i = 0; i < 10; i++) {
+		System.out.println("222222222");
+	}
+}
+}
