89e976ae6863a09a8473b74dd3156842f47e5055
jose.rubinc.1 <jose.rubinc.1@ED34007007P395.educa.jcyl.es>
2024-11-29 08:46:04 +0100

    WAAAAAAAA

--- WORKSPACE-PROG/src/cadenas/PPT6ACT3.java
+++ WORKSPACE-PROG/src/cadenas/PPT6ACT3.java
@@ -1,5 +1,14 @@
 package cadenas;
 
 public class PPT6ACT3 {
+	public static void main (String[]args) {
+        String entrada = "casa blanca";
+        String salida = "";
 
+        for (int i = entrada.length() - 1; i >= 0; i--) {
+            salida += entrada.charAt(i);
+        }
+
+        System.out.println(salida);
+    }
 }
---
+++ WORKSPACE-PROG/src/cadenas/PPTACT4.java
@@ -0,0 +1,14 @@
+package cadenas;
+import java.util.Iterator;
+import java.util.Scanner;
+public class PPTACT4 {
+	public static void main(String[]args) {
+		Scanner entrada = new Scanner (System.in);
+		System.out.println("Escribe una frase: ");
+		String cadena = entrada.nextLine();
+		int i, pos=0;
+		
+		for (i = 0; i < cadena.length(); i++)
+			;
+	}
+}
